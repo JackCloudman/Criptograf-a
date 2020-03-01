37c23c83c5010f1dece511e13a5c943070916434
Jack Cloudman <maylo360xd@gmail.com>
2020-03-01 11:43:31 -0600

    read file, encrypt and decrypt

--- DES/main.go
+++ DES/main.go
@@ -1,30 +1,37 @@
 package main
 
-import "log"
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+)
 
 func main() {
+	key := []bool{true, false, true, false, false, false, false, false, true, false}
+	filename := "test.txt"
 	// Weas para leer de un archivo y leer byte por byte
-	/*data, err := ioutil.ReadFile("test.txt")
-	arrxor := []bool{true, false, true, false, true, true, false, false}
-	for _, i := range data {
-		fmt.Println(i)
-		arr := getArr(i, 8)
-		result, err := BinXOR(arrxor, arr)
-		if err != nil {
-			return
-		}
-		fmt.Println(result)
-		tabla := []int{0, 0, 6, 2, 5}
-		fmt.Println(Permutar(result, tabla))
-	}
+	data, err := ioutil.ReadFile(filename)
 	if err != nil {
 		fmt.Println("File reading error", err)
 		return
-	}*/
-	key := []bool{true, false, true, false, false, false, false, false, true, false}
-	message := []bool{true, false, true, true, true, true, false, true}
-	Cmessage := DesEncrypt(message, key)
-	Dmessage := DesDecrypt(Cmessage, key)
-	log.Println(Cmessage)
-	log.Println(Dmessage)
+	}
+	f, err := os.Create(filename + ".des")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	for _, i := range data {
+		imessage := getArr(i, 8)
+		result := getInt(DesEncrypt(imessage, key))
+		writeFile(f, []byte{byte(result)})
+	}
+	/*
+		message := []bool{true, false, true, true, true, true, false, true}
+		Cmessage := DesEncrypt(message, key)
+		Dmessage := DesDecrypt(Cmessage, key)
+		log.Println(Cmessage)
+		log.Println(Dmessage)*/
+}
+func writeFile(f *os.File, data []byte) {
+	f.Write(data)
 }
